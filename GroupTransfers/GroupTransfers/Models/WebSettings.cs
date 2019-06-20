3e0710dc98c077f4872a726d256fa248be40d070
millennialssystem <millennialssystem@gmail.com>
2019-06-20 11:00:20 -0400

    20/01/2019 11:00 AM
    Inclusion del nivel de servicios

--- GroupTransfers/GroupTransfers/Models/WebSettings.cs
+++ GroupTransfers/GroupTransfers/Models/WebSettings.cs
@@ -5,18 +5,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Protocols;
-
+using GroupTransfers.Services;
 namespace GroupTransfers.Models
 {
     public class WebSettings: PageModel
     {
+        private MSutils MSutil;
         public int Versionjs { get; set; }
         public int Versioncss { get; set; }
         public WebSettings()
         {
-            Versionjs = (int)DateTime.Now.Ticks;
-            Versioncss = 5;
-
+            MSutil = new MSutils("");
+            Versionjs = MSutil.GetversionJs();
+            Versioncss = MSutil.GetversionCss();
         }
     }
 }
---
+++ GroupTransfers/GroupTransfers/Services/MSutils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GroupTransfers.Services
+{
+    public struct MSutils
+    {
+        private MySqlConnection ConnectionString;
+        public MSutils(string connectionString)
+        {           
+            connectionString = "SERVER=localhost;" + "DATABASE=gt;" + "UID=root;" + "PASSWORD=;";
+
+            ConnectionString = new MySqlConnection(connectionString);
+            ConnectionString.Open();
+        }
+        public int GetversionJs()
+        {
+            try
+            {
+                return 5;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+        public int GetversionCss()
+        {
+            try
+            {
+                return 5;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
