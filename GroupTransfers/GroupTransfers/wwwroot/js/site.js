2f363fc6208fa50b98c45043cb50d4155b740af7
millennialssystem <millennialssystem@gmail.com>
2019-06-28 22:21:18 -0400

    cambios para el administrador de tasas de cambio

--- GroupTransfers/GroupTransfers/wwwroot/js/site.js
+++ GroupTransfers/GroupTransfers/wwwroot/js/site.js
@@ -4,6 +4,7 @@ const constTosell = 0.9487666034155598;
 $(document).ready(function (e) {
     Init();
     Initcurrencyprice(document.getElementById("valuescurrencyprice").value, document.getElementById("currencypricetbody"), document.getElementById("lastupdatelable"), document.getElementById("typemoneda"));
+    InitcurrencyManagerPrice(document.getElementById("valuescurrencyprice").value, document.getElementById("managerPrice"));
 });
 function Init() {
     var el = document.getElementById("tosell");
@@ -55,7 +56,7 @@ function Initcurrencyprice(tasas, tblBody, lastupdatelable, typemoneda) {
 
         celda = document.createElement("th");
         celda.scope = "row";
-        textoCelda = document.createTextNode(item.prc_name);
+        textoCelda = document.createTextNode("(" + item.prc_bank + ")" + item.prc_name);
         celda.appendChild(textoCelda);
         fila.appendChild(celda);
 
@@ -76,11 +77,76 @@ function Initcurrencyprice(tasas, tblBody, lastupdatelable, typemoneda) {
     transferirdivisas();
 }
 
+function InitcurrencyManagerPrice(tasas, managerPrice) {
+
+    JSON.parse(tasas).forEach(function (item) {
+        debugger;
+        var row = document.createElement("div");
+        row.className = "row p-2";
+        row.id = "rowManagerPrice" + item.prc_id;
+        /*Begin Bank */
+        var bankDiv = document.createElement("div");
+        bankDiv.className = "col-sm";
+        var bank = document.createElement("input");
+        bank.id = "bankManagerPrice" + item.prc_id;
+        bank.type = "text";
+        bank.className = "form-control";
+        bank.value = item.prc_bank;
+        bankDiv.appendChild(bank);
+        /*End Bank */
+
+        /*Begin coin */
+        var coinDiv = document.createElement("div");
+        coinDiv.className = "col-sm";
+        var coin = document.createElement("input");
+        coin.id = "coinManagerPrice" + item.prc_id;
+        coin.type = "text";
+        coin.className = "form-control";
+        coin.value = item.prc_name;
+        coinDiv.appendChild(coin);
+        /*End coin */
+
+        /*Begin value */
+        var valueDiv = document.createElement("div");
+        valueDiv.className = "col-sm";
+        var value = document.createElement("input");
+        value.id = "valueManagerPrice" + item.prc_id;
+        value.type = "text";
+        value.className = "form-control";
+        value.value = item.prc_value;
+        valueDiv.appendChild(value);
+        /*End value */
+
+
+        /*Begin Disable*/
+        var desactivar = document.createElement("div");
+        desactivar.className = "col col-lg-2";
+        var btnDesactivar = document.createElement("button");
+        btnDesactivar.id = "rowManagerPrice" + item.prc_id;
+        btnDesactivar.type = "button";
+        btnDesactivar.className = "btn btn-danger";
+        var textDesactivar = document.createTextNode("Eliminar");
+        btnDesactivar.appendChild(textDesactivar);
+        desactivar.appendChild(btnDesactivar);
+        /*End Disable*/
+
+        managerPrice.appendChild(row);
+        row.appendChild(bankDiv);
+        row.appendChild(coinDiv);
+        row.appendChild(valueDiv);
+        row.appendChild(desactivar);
+
+    });
+
+}
+
+
+
 function transferirdivisas() {
     var moneda = document.getElementById("typemoneda").value;
     var cantidad = document.getElementById("cantidadmoneda").value;
     var regex = new RegExp("[0-9]");
-    document.getElementById("transferirdivisas").innerHTML ="Bs: " + ( moneda * cantidad);
+    document.getElementById("transferirdivisas").innerHTML = "Bs: " + (moneda * cantidad);
 }
 
 function completeRight(cadena, item, length) {
